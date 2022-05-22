2c23a5aed0e1e78960940a6aea1ca6eece0f9cb7
akilani <alaa.kilani@gmail.com>
2022-05-22 16:24:24 +0300

    Delete infra/cluster directory

--- infra/cluster/cloudformation/command.sh
+++
@@ -1,6 +0,0 @@
-aws cloudformation deploy \
-    --template-file efs.yaml \
-    --stack-name teedy-meduim-stack \
-    --tags file://tags.json \
-    --region "us-west-1" \
-    --parameter-overrides file://params.json
